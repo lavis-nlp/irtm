8023d27a97224edb687b60a4dca4959a3a2ea4c0
Felix Hamann <felix@hamann.xyz>
2020-08-11 16:45:24 +0200

    added first set of Model.predict* methods

--- ryn/embers/keen.py
+++ ryn/embers/keen.py
@@ -15,16 +15,17 @@ from ryn.common import logging
 
 import json
 import copy
+import pickle
 import random
 import pathlib
 import textwrap
 
 from datetime import datetime
 from functools import partial
+from functools import lru_cache
 from dataclasses import dataclass
 
 import torch
-
 import numpy as np
 import pandas as pd
 
@@ -33,6 +34,7 @@ from pykeen import triples as keen_triples
 from pykeen.models import base as keen_base
 
 from typing import Any
+from typing import Set
 from typing import Dict
 from typing import List
 from typing import Union
@@ -42,6 +44,104 @@ from typing import Collection
 
 log = logging.get('embers.keen')
 DATEFMT = '%Y.%m.%d.%H%M%S.%f'
+
+
+# ---
+# might re-introduce the helper.file_cache at some point...
+
+
+def _cached_predictions(predict_all):
+    def _inner(self: split.Dataset, e: int):
+        path = ryn.ENV.CACHE_DIR / 'embers.keen'
+        path.mkdir(exist_ok=True, parents=True)
+        path /= f'{self.uri}.{e}.pkl'
+
+        log.info(f'looking for {path}')
+        if path.is_file():
+            with path.open(mode='rb') as fd:
+                return pickle.load(fd)
+
+        log.info(f'! cache miss for {path}')
+        res = predict_all(self, e=e)
+
+        log.info(f'saving to {path}')
+        with path.open(mode='wb') as fd:
+            pickle.dump(res, fd)
+
+        return res
+
+    return _inner
+
+
+# ---
+
+
+# FIXME
+# COPIED FROM PYKEEN BRANCH "improve-novelty-computation"
+# REMOVE THIS METHOD WHEN PULL REQUEST 51 IS MERGED
+
+
+TRIPLES_DF_COLUMNS = [
+    'head_id', 'head_label',
+    'relation_id', 'relation_label',
+    'tail_id', 'tail_label']
+
+
+# self is a triples factory
+def tensor_to_df(self, tensor: torch.LongTensor, **kwargs) -> pd.DataFrame:
+    """
+
+    Take a tensor of triples and make a pandas dataframe with labels.
+
+    :param tensor: shape: (n, 3)
+        The triples, ID-based and in format
+        (head_id, relation_id, tail_id).
+
+    :return:
+        A dataframe with n rows, and 6 + len(kwargs) columns.
+    """
+    # Input validation
+    additional_columns = set(kwargs.keys())
+    forbidden = additional_columns.intersection(TRIPLES_DF_COLUMNS)
+    if len(forbidden) > 0:
+        raise ValueError(
+            f'The key-words for additional arguments must not be in '
+            f'{TRIPLES_DF_COLUMNS}, but {forbidden} were '
+            f'used.'
+        )
+
+    # convert to numpy
+    tensor = tensor.cpu().numpy()
+    data = dict(zip(['head_id', 'relation_id', 'tail_id'], tensor.T))
+
+    # vectorized label lookup
+    entity_id_to_label = np.vectorize(
+        {v: k for k, v in self.entity_to_id.items()}.__getitem__)
+    relation_id_to_label = np.vectorize(
+        {v: k for k, v in self.relation_to_id.items()}.__getitem__)
+
+    for column, id_to_label in dict(
+        head=entity_id_to_label,
+        relation=relation_id_to_label,
+        tail=entity_id_to_label,
+    ).items():
+        data[f'{column}_label'] = id_to_label(data[f'{column}_id'])
+
+    # Additional columns
+    for key, values in kwargs.items():
+        # convert PyTorch tensors to numpy
+        if torch.is_tensor(values):
+            values = values.cpu().numpy()
+        data[key] = values
+
+    # convert to dataframe
+    rv = pd.DataFrame(data=data)
+
+    # Re-order columns
+    columns = TRIPLES_DF_COLUMNS + sorted(
+        set(rv.columns).difference(TRIPLES_DF_COLUMNS))
+
+    return rv.loc[:, columns]
 
 
 # ---
@@ -224,16 +324,15 @@ class Model:
         return self.parameters['model']
 
     @property
+    def uri(self) -> str:
+        return (
+            f'{self.dataset.name}_'
+            f'{self.name}_'
+            f'{self.timestamp.strftime(DATEFMT)}')
+
+    @property
     def dimensions(self) -> int:
         return self.parameters['model_kwargs']['embedding_dim']
-
-    @property
-    def e2id(self) -> Dict[str, int]:
-        return self.keen.triples_factory.entity_to_id
-
-    @property
-    def r2id(self) -> Dict[str, int]:
-        return self.keen.triples_factory.relation_to_id
 
     @property
     def metrics(self) -> pd.DataFrame:
@@ -258,6 +357,30 @@ class Model:
             f'Trained: {self.timestamp.strftime("%d.%m.%Y %H:%M")}\n'
             f'Dataset: {self.metadata["dataset_name"]}\n\n'
             f'{self.metrics}\n', '  ')
+
+    def __hash__(self) -> int:
+        return hash(self.uri)
+
+    # ---
+
+    def e2s(self, e: int) -> str:
+        return e2s(self.dataset.g, e)
+
+    def r2s(self, r: int) -> str:
+        return r2s(self.dataset.g, r)
+
+    def e2id(self, e: int) -> int:
+        return self.keen.triples_factory.entity_to_id[self.e2s(e)]
+
+    def r2id(self, r: int) -> int:
+        return self.keen.triples_factory.relation_to_id[self.r2s(r)]
+
+    @property
+    @lru_cache
+    def mapped_train_triples(self) -> Set[Tuple[int]]:
+        return set(map(
+            lambda t: tuple(t.tolist()),
+            self.keen.triples_factory.mapped_triples))
 
     #
     # ---  PYKEEN ABSTRACTION
@@ -290,13 +413,133 @@ class Model:
 
         return [float(t) for t in scores]
 
-    def predict_heads(self, t: int, r: int, **kwargs) -> pd.DataFrame:
+    def predict_heads(
+            self, *,
+            t: int = None,
+            r: int = None,
+            **kwargs) -> pd.DataFrame:
+        """
+
+        See keen_base.Model.predict_heads.
+
+        Parameters
+        ----------
+
+        t: int
+          tail entity id (using graph.Graph indexes)
+
+        r: int
+          relation id (using graph.Graph indexes)
+
+        """
         tstr, rstr = e2s(self.dataset.g, t), r2s(self.dataset.g, r)
         return self.keen.predict_heads(rstr, tstr, **kwargs)
 
-    def predict_tails(self, h: int, r: int, **kwargs):
+    def predict_tails(
+            self, *,
+            h: int = None,
+            r: int = None,
+            **kwargs) -> pd.DataFrame:
+        """
+
+        See keen_base.Model.predict_tails
+
+        Parameters
+        ----------
+
+        h: int
+          head entity id (using graph.Graph indexes)
+
+        r: int
+          relation id (using graph.Graph indexes)
+
+        """
         hstr, rstr = e2s(self.dataset.g, h), r2s(self.dataset.g, r)
         return self.keen.predict_tails(hstr, rstr, **kwargs)
+
+    # @_cached_predictions
+    def _predict_all(self, e: int, tails: bool) -> pd.DataFrame:
+
+        # FIXME h=1333 (warlord) is unknown
+
+        # awaiting https://github.com/pykeen/pykeen/pull/51
+
+        e = self.e2id(e)
+
+        def _to_tensor(dic):
+            return torch.Tensor(list(dic.values()))
+
+        rids = _to_tensor(self.keen.triples_factory.relation_to_id)
+        eids = _to_tensor(self.keen.triples_factory.entity_to_id)
+
+        target = dict(dtype=torch.long, device=self.keen.device)
+
+        # either hr_batch or rt_batch
+        batch = torch.zeros((self.keen.num_relations, 2)).to(**target)
+        batch[:, 0], batch[:, 1] = (e, rids) if tails else (rids, e)
+
+        if tails:
+            scorer = self.keen.predict_scores_all_tails
+        else:
+            scorer = self.keen.predict_scores_all_heads
+
+        # entities seem to be returned by insertion order in the triple factory
+        # https://github.com/pykeen/pykeen/blob/a2ffc2c278bbb0371cc1e056d6b34729a469df54/src/pykeen/models/base.py#L623
+        y = scorer(batch).detach()
+        assert y.shape == (self.keen.num_relations, self.keen.num_entities)
+
+        res = torch.zeros((len(rids), len(eids), 3), dtype=torch.long)
+        res[:, :, 0 if tails else 2] = e
+
+        for i, r in enumerate(rids):
+            res[i, :, 2 if tails else 0] = eids
+            res[i, :, 1] = r
+
+        # build dataframe
+
+        n = len(rids) * len(eids)
+        res = res.view(n, 3)
+
+        in_train = list(map(
+            lambda t: tuple(t.tolist()) in self.mapped_train_triples,
+            res))
+
+        _to_df = partial(tensor_to_df, self.keen.triples_factory)
+        df = _to_df(res, scores=y.view((n, )), train=in_train, )
+
+        df = df.sort_values(by='scores', ascending=False)
+
+        return df
+
+    def predict_all_tails(self, *, h: int = None) -> pd.DataFrame:
+        """
+
+        Predict all possible (r, t) for the given h
+
+        Parameters
+        ----------
+
+        h: int
+          entity id (using graph.Graph indexes)
+
+        """
+        assert h is not None
+        return self._predict_all(h, True)
+
+    def predict_all_heads(self, *, t: int = None) -> pd.DataFrame:
+        """
+
+        Predict all possible (h, r) for the given t
+
+        Parameters
+        ----------
+
+        t: int
+          entity id (using graph.Graph indexes)
+
+        """
+        assert t is not None
+        return self._predict_all(t, False)
 
     # ---
 
