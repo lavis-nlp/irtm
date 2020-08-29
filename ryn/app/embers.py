535713c8aae9ae287c8598dd4bd58367dde92de1
Felix Hamann <felix@hamann.xyz>
2020-08-29 11:28:12 +0200

    pykeen update; new data format

--- ryn/app/embers.py
+++ ryn/app/embers.py
@@ -5,6 +5,7 @@ from ryn import app
 from ryn.embers import keen
 from ryn.common import logging
 
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -88,15 +89,17 @@ def load_keen(path) -> keen.Model:
 def keen_to_row(model: keen.Model):
     kind = 'avg'
 
+    st.write(f'### {model.dataset.path.name} {model.timestamp}')
+
     metrics = model.results['metrics']
-    hits = metrics['hits_at_k'][kind]
+    hits = metrics['hits_at_k']['both'][kind]
 
     return (
         model.name,
         model.dimensions,
         model.timestamp,
-        metrics['mean_rank'][kind],
-        metrics['mean_reciprocal_rank'][kind],
+        metrics['mean_rank']['both'][kind],
+        metrics['mean_reciprocal_rank']['both'][kind],
         hits['1'], hits['3'], hits['5'], hits['10'],
     )
 
@@ -132,28 +135,59 @@ class Context(app.Context):
         # ---
 
         st.write(f'## {model.name}-{model.dimensions}')
-        st.write(f'Dataset: {model.metadata["dataset_name"]}')
-        st.write(f'Date: {model.timestamp.strftime("%d.%m.%Y %H:%M")}')
 
         times = model.results['times']
-        st.write(f'Training: {timedelta(seconds=times["training"])}')
-        st.write(f'Evaluation: {timedelta(seconds=times["evaluation"])}')
-
-        stopped = model.results["stopper"]["stopped"]
-        st.write(f'Stopped early: {stopped}')
+        st.table(pd.DataFrame({
+            'Dataset': model.metadata["dataset_name"],
+            'Date': model.timestamp.strftime("%d.%m.%Y %H:%M"),
+            'Training': timedelta(seconds=times["training"]),
+            'Evaluation': timedelta(seconds=times["evaluation"]),
+            'Stopped early': model.results["stopper"]["stopped"],
+        }, index=[0]).transpose())
 
         # ---
 
         st.write('### Evaluation')
         st.write(
             'Adjusted mean rank: '
-            f'{model.results["metrics"]["adjusted_mean_rank"]:3f}')
+            f'{model.results["metrics"]["adjusted_mean_rank"]["both"]:3f}')
         st.dataframe(model.metrics)
 
         # ---
 
         st.write('### Training')
-        st.line_chart(model.results['losses'])
+
+        # unpack
+
+        hits_at_k = model.results['metrics']['hits_at_k']['both']['avg']
+        stopper_freq = model.parameters['stopper_kwargs']['frequency']
+        loss_train = model.results['losses']
+        valid_results = model.results['stopper']['results']
+
+        # losses
+
+        st.write('Training')
+        st.line_chart({'training loss': loss_train})
+
+        # validation
+
+        # they use hits@10 as default validation metric
+        # https://github.com/pykeen/pykeen/blob/22f8815fe6520dc89f786e43d5cb3682abe1113d/src/pykeen/evaluation/rank_based_evaluator.py#L173
+
+        st.write('Validation')
+
+        valid_interp = np.interp(
+            range(len(loss_train)),
+            range(1, len(loss_train), stopper_freq),
+            valid_results)
+
+        data = {
+            'hits@10 valid': valid_interp,
+            'hits@10 test': np.repeat(hits_at_k['10'], len(valid_interp)),
+        }
+        st.line_chart(data)
+
+        # ---
 
         st.write('### Parameters')
         st.write(model.parameters)
