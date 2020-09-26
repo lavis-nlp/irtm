dbcef6a1957043d2c6d7db24879d4f7328276ebb
Felix Hamann <felix@hamann.xyz>
2020-09-26 10:30:56 +0200

    optionally load database to memory

--- ryn/text/loader.py
+++ ryn/text/loader.py
@@ -3,13 +3,23 @@
 
 import sqlite3
 
+from ryn.common import helper
+from ryn.common import logging
+
+from functools import partial
 from contextlib import closing
 from dataclasses import dataclass
 
-from ryn.common import helper
+from tqdm import tqdm as _tqdm
 
 from typing import Tuple
 from typing import Generator
+
+
+log = logging.get('text.loader')
+
+
+tqdm = partial(_tqdm, ncols=80)
 
 
 @helper.notnone
@@ -107,9 +117,24 @@ class SQLite:
 
     # ---
 
-    def __init__(self, *, database: str = None):
-        self._conn = sqlite3.connect(database)
-        self._cursor = self._conn.cursor()
+    def __init__(self, *, database: str = None, to_memory: bool = False):
+        log.info(f'connecting to database {database}')
+
+        if to_memory:
+            log.info('copying database to memory')
+            self._conn = sqlite3.connect(':memory:')
+            self._cursor = self._conn.cursor()
+
+            log.info(f'opening {database}')
+            with sqlite3.connect(database) as con:
+                # bar = partial(tqdm,  desc='copy to memory', unit=' statements')  # noqa
+                for sql in con.iterdump():
+                    self._cursor.execute(sql)
+
+        else:
+            log.info('accessing database from disk')
+            self._conn = sqlite3.connect(database)
+            self._cursor = self._conn.cursor()
 
     def __enter__(self):
         return SQLite.Selector(conn=self._conn, cursor=self._cursor)
