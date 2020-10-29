9ea3348cf28188245dc7e3a2a867464d5b870b56
Felix Hamann <felix@hamann.xyz>
2020-10-29 15:22:37 +0100

    moved configuration to conf/kgc

--- ryn/kgc/trainer.py
+++ ryn/kgc/trainer.py
@@ -2,8 +2,8 @@
 
 import ryn
 from ryn.kgc import keen
-from ryn.kgc import config
 from ryn.kgc import pipeline
+from ryn.kgc.config import Config
 from ryn.graphs import split
 from ryn.common import helper
 from ryn.common import logging
@@ -17,86 +17,25 @@ log = logging.get('kgc.trainer')
 @helper.notnone
 def train(
         *,
-        model: str = None,
+        base: Config = None,
         split_dataset: split.Dataset = None,
         keen_dataset: keen.Dataset = None,
         offline: bool = False,
 ) -> None:
 
-    conf = config.Config(
-        general=config.General(
-            dataset=split_dataset.name,
-        ),
-        optuna=config.Optuna(
-            study_name=f'{model}-keen-sweep',
-            trials=100,
-            maximise=True,  # hits@10
-        ),
-        tracker=config.Tracker(
-            cls='wandb',
-            project='ryn-keen',
-            experiment=f'{keen_dataset.name}-{model}',
-            reinit=True,
-            offline=offline,
-        ),
-        model=config.Model(
-            cls=model,
-            embedding_dim=config.IntSuggestion(
-                low=50, high=500, step=50, initial=250),
-            automatic_memory_optimization=True,
-            preferred_device='cuda',
-        ),
-        optimizer=config.Optimizer(
-            cls='Adagrad',
-            lr=config.FloatSuggestion(
-                low=1e-3, high=1e-1, log=True, initial=1e-2),
-        ),
-        regularizer=config.Regularizer(
-            cls='LpRegularizer',
-            weight=config.FloatSuggestion(low=1e-2, high=1e-1, log=True),
-            p=2.0,
-            normalize=True,
-        ),
-        loss=config.Loss(
-            cls='MarginRankingLoss',
-            margin=1.0,
-            reduction='mean',
-        ),
-        evaluator=config.Evaluator(
-            cls='RankBasedEvaluator',
-        ),
-        stopper=config.Stopper(
-            cls='EarlyStopper',
-            frequency=20,
-            patience=20,
-            relative_delta=1e-2,
-        ),
-        training_loop=config.TrainingLoop(
-            cls='SLCWATrainingLoop',
-        ),
-        sampler=config.Sampler(
-            cls='BasicNegativeSampler',
-            num_negs_per_pos=config.IntSuggestion(
-                low=1, high=20, step=1, initial=5),
-        ),
-        training=config.Training(
-            num_epochs=2000,
-            batch_size=250,
-        ),
-    )
-
     time = str(datetime.now()).replace(' ', '_')
-    out = ryn.ENV.KGC_DIR / split_dataset.name / f'{model}-{time}'
+    out = ryn.ENV.KGC_DIR / split_dataset.name / f'{base.model.cls}-{time}'
+    base.save(out)
 
     pipeline.multi(
-        base=conf,
         out=out,
+        base=base,
         split_dataset=split_dataset,
         keen_dataset=keen_dataset)
 
 
 def train_from_cli(
-        model: str = None,
+        config: str = None,
         split_dataset: str = None,
         offline: bool = False):
     log.info('running training from cli')
@@ -112,8 +51,11 @@ def train_from_cli(
 
     print(f'\n{split_dataset}\n{keen_dataset}\n')
 
+    base = Config.load(config)
+    base.general.dataset = split_dataset.name
+
     train(
-        model=model,
+        base=base,
         split_dataset=split_dataset,
         keen_dataset=keen_dataset,
         offline=offline)
