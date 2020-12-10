9c9016c311fccb4e1d74da5bdabc862218bdc0f0
Felix Hamann <felix@hamann.xyz>
2020-12-10 17:07:12 +0100

    blackened

--- conf/resolve/entity2label.py
+++ conf/resolve/entity2label.py
@@ -6,10 +6,11 @@ import argparse
 
 
 def main(args):
+    # this assumes the mid to be unique
 
     m2i = {}  # mid -> idx
 
-    with open(args.e2i, mode='r') as fd:
+    with open(args.e2i, mode="r") as fd:
         fd.readline()
 
         for line in fd:
@@ -18,36 +19,30 @@ def main(args):
 
     m2l = {}  # mid -> label
 
-    with open(args.e2w, mode='r') as fd:
+    with open(args.e2w, mode="r") as fd:
         data = json.load(fd)
         for mid in data:
-            m2l[mid] = data[mid]['label']
+            m2l[mid] = data[mid]["label"]
 
-    with open(args.out, mode='w') as fd:
-        fd.write(f'{len(m2i)}\n')
+    with open(args.out, mode="w") as fd:
+        fd.write(f"{len(m2i)}\n")
 
         for mid, idx in m2i.items():
-            fd.write(f'{m2l.get(mid, mid)} {idx}\n')
+            fd.write(f"{m2l.get(mid, mid)} {idx}\n")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        'e2i', type=str,
-        help='the entity2id file')
+    parser.add_argument("e2i", type=str, help="the entity2id file")
 
-    parser.add_argument(
-        'e2w', type=str,
-        help='the entity2wikidata file')
+    parser.add_argument("e2w", type=str, help="the entity2wikidata file")
 
-    parser.add_argument(
-        'out', type=str,
-        help='target text file')
+    parser.add_argument("out", type=str, help="target text file")
 
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     main(args)
